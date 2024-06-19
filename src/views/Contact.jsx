b9e957166309ea9f3f7be27c2bf45c765b94f1bd
Ambrosio <ambrosio.rhc@gmail.com>
2024-06-19 12:02:07 -0400

    ok

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,15 +1,16 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Importa Link para la navegación
 import { Context } from '../store/appContext';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import '../App.css';
 
 const Contact = () => {
-  const { state } = useContext(Context); // Obtener el estado del contexto
+  const { state } = useContext(Context);
 
-  // Use useEffect para imprimir el estado en consola al cargar el componente
   useEffect(() => {
     console.log('Estado actual de los contactos:', state.contacts);
   }, [state.contacts]);
 
-  // Verificar que state.contacts sea un array antes de mapearlo
   if (!Array.isArray(state.contacts)) {
     console.error('state.contacts no es un array:', state.contacts);
     return (
@@ -21,11 +22,28 @@ const Contact = () => {
   }
 
   return (
-    <div>
+    <div className="contact-list">
       <h1>Lista de Contactos</h1>
+      <Link to="/add" className="add-button">Agregar Contacto</Link>
       <ul>
         {state.contacts.map(contact => (
-          <li key={contact.id}>{contact.name}</li>
+          <li key={contact.id} className="contact-item">
+            <div className="contact-info">
+              <div className="contact-avatar">
+                <img src="https://picsum.photos/200/300" alt="Avatar" className="avatar" />
+              </div>
+              <div className="contact-details">
+                <h2>{contact.name}</h2>
+                <p><strong>Dirección:</strong> {contact.address}</p>
+                <p><strong>Teléfono:</strong> {contact.phone}</p>
+                <p><strong>Email:</strong> {contact.email}</p>
+              </div>
+            </div>
+            <div className="contact-actions">
+              <button className="action-button edit-button"><FaEdit /></button>
+              <button className="action-button delete-button"><FaTrash /></button>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
